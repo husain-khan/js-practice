22cd1c862e5df03397ebc994336e067e9e9fc16d
husain-khan <136927986+husain-khan@users.noreply.github.com>
2024-03-17 11:18:45 +0000

    02_varibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 93392;
+let accountEmail="khanhn2@gmail.com";
+var accountPassword = "husainKhan";
+accountCity = "nagpur";
+let accountState;
+//accoutnId = 2; 
+
+console.log(accountId);
+
+//node 01_basics/01_variables.js
+accountEmail="husainkhan@gaml.com"
+accountPassword="khan"
+accountCity="pune"
+
+
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState])
+
+//variables can be defined two times let and var 
+//we will not use var 
+//because it does not know scope {block and  functional scope}
+//do not use var any time **** 
