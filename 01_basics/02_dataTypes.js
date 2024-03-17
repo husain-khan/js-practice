47181e16650342e18adf978de7427f23d3677187
husain-khan <136927986+husain-khan@users.noreply.github.com>
2024-03-17 11:19:11 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"Use strict";// treat all js code as newer version 
+
+//alert(3 + 3); // we are using node.js not browser
+
+//node 01_basics/02_dataTypes.js
+
+console.log(3+33);
+//code readiablity should be high 
+//mdn documentattion
+
+let name="husain" //string
+let age =26  //integer 
+let isLoggedIn=false
+let temperature = null
+//number 
+//bigint 
+//string 
+//boolean =>true /false
+//null  =>stand alone value 
+// undefined => not assing any value 
+//symbol =>unique
+//chsabv
+
+
+//object
+console.log(typeof "husian")
+console.log(typeof null)//null object
