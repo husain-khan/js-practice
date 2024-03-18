fbf2a6a1438745e1c4b52124f8e608c251f443ba
husain-khan <136927986+husain-khan@users.noreply.github.com>
2024-03-18 14:47:10 +0000

    comparisonof Datatype

---
+++ 01_basics/04_comparion.js
@@ -0,0 +1,14 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+//not prfered compariosion of same data type
+
+console.log( null >0);//avoid this code
+console.log(null==0 );//avoid  this code 
+console.log(null>=0);
+console.log(undefined ==0);//all false case
+//comparion and equality check is two diffrent thing
+
+//strict check "===="
+console.log("2"==2);
+console.log("2"===2);
