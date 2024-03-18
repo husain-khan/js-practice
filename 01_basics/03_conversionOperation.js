1538957bf3b26aa54f8ba9d36e793fcb389aa000
husain-khan <136927986+husain-khan@users.noreply.github.com>
2024-03-18 14:27:13 +0000

    conversion_datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score ="80abc"
+//this string cannot be convert so convert karne ke baad value type nan hai
+console.log(typeof(score))
+//const {score } = re 
+
+
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+let score1 ="null"
+console.log(typeof(score1))
+//const {score } = re 
+
+
+let valueInNumber1= Number(score1)
+console.log(typeof(valueInNumber1))
+console.log(valueInNumber1)
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true
+//"emptystring"=>false
+
+let someNumber =33
+ 
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
